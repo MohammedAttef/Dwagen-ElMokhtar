c2ac656ae4729b512cb26b50260aee0a9a3933db
MohammedAttef <bondoqfosdoq@gmail.com>
2026-02-22 23:29:00 +0200

    Project cleaning

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -14,11 +14,11 @@ export class Home implements OnInit {
   private metaService = inject(Meta);
 
   ngOnInit() {
-    this.titleService.setTitle('مزارعنا | أجود أنواع الدواجن الطازجة والمجمدة');
+    this.titleService.setTitle('دواجن المختار | أجود أنواع الدواجن الطازجة والمجمدة');
     this.metaService.addTags([
-      { name: 'description', content: 'نقدم لكم أفضل أنواع الدواجن الطازجة والمجمدة بأعلى معايير الجودة والنظافة. اطلب الآن من مزارعنا وتوصيل سريع.' },
-      { name: 'keywords', content: 'دواجن, دجاج طازج, فراخ مجمدة, مزارعنا, فراخ بانيه, توصيل دواجن' },
-      { property: 'og:title', content: 'مزارعنا | أجود أنواع الدواجن الطازجة' },
+      { name: 'description', content: 'نقدم لكم أفضل أنواع الدواجن الطازجة والمجمدة بأعلى معايير الجودة والنظافة. اطلب الآن من دواجن المختار وتوصيل سريع.' },
+      { name: 'keywords', content: 'دواجن, دجاج طازج, فراخ مجمدة, دواجن المختار, فراخ بانيه, توصيل دواجن' },
+      { property: 'og:title', content: 'دواجن المختار | أجود أنواع الدواجن الطازجة' },
       { property: 'og:description', content: 'نقدم لكم أفضل أنواع الدواجن الطازجة والمجمدة بأعلى معايير الجودة.' },
     ]);
   }
